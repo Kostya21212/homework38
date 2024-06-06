5b3f5c779382fdd8844a623002c854592469aab8
Onischenko Kostyantyn <Onischenko411@gmail.com>
2024-06-06 11:46:50 +0300

    Save

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
 const rename = require('gulp-rename');
 const uglify = require('gulp-uglify');
 const cleanCSS = require('gulp-clean-css');
 
-
 gulp.task('sass', function() {
     return gulp.src('css/*.scss')
         .pipe(sass())
+        .pipe(postcss([autoprefixer()])) // Применяем автопрефиксы с помощью postcss
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('css'))
@@ -29,7 +31,7 @@ gulp.task('serve', function() {
     });
 
     gulp.watch('css/*.scss', gulp.series('sass'));
-    gulp.watch('js/*.js', gulp.series('scripts'));
+    gulp.watch('js.js/*.js', gulp.series('scripts'));
     gulp.watch('*.html').on('change', browserSync.reload);
 });
 
