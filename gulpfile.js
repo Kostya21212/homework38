abdd239f998f6700ef8369f34b567e43bfa938c4
Onischenko Kostyantyn <Onischenko411@gmail.com>
2024-06-06 11:41:48 +0300

    222

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,36 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass')); // Указываем компилятор явно
+const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const cleanCSS = require('gulp-clean-css');
 
-// Компиляция SCSS в CSS
+
 gulp.task('sass', function() {
     return gulp.src('css/*.scss')
         .pipe(sass())
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('css'))
         .pipe(browserSync.stream());
 });
 
-// Запуск сервера и слежение за изменениями
+gulp.task('scripts', function() {
+    return gulp.src('js.js/*.js')
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('js'))
+        .pipe(browserSync.stream());
+});
+
 gulp.task('serve', function() {
     browserSync.init({
         server: './'
     });
 
     gulp.watch('css/*.scss', gulp.series('sass'));
+    gulp.watch('js/*.js', gulp.series('scripts'));
     gulp.watch('*.html').on('change', browserSync.reload);
 });
 
-// Задача по умолчанию
-gulp.task('default', gulp.series('sass', 'serve'));
+gulp.task('default', gulp.parallel('sass', 'scripts', 'serve'));
